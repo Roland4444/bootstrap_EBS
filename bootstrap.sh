adabd04c3599cd630c814fcf9122d200b72d5455
Roland4444 <turbopollux@rambler.ru>
2019-03-29 10:36:05 +0400

    update script

--- bootstrap.sh
+++ bootstrap.sh
@@ -1,6 +1,7 @@
 sudo add-apt-repository ppa:openjdk-r/ppa
 sudo apt update
 sudo apt install git
+sudo apt install openjdk-8-jdk
 sudo apt install openjdk-11-jdk
 #wget https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz
 #tar xf  openjdk-11.0.1_linux-x64_bin.tar.gz
