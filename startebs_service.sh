0bcea371843be0fad68457267e4caa625e06a0e0
Roland4444 <turbopollux@rambler.ru>
2019-04-16 14:06:53 +0400

    Update starebs

--- startebs_service.sh
+++ startebs_service.sh
@@ -1,2 +1,4 @@
 cd mebs
-java -XX:+UnlockExperimentalVMOptions -XX:+UseEpsilonGC --module-path build -m eu.roland.ebs/service.Service_JAKtor
+java --module-path build -m eu.roland.ebs/service.Service_JAKtor
+
+
