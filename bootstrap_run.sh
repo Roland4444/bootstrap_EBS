d01080684e12c1751ef8b9716f692adf40c1ddb0
Roland4444 <turbopollux@rambler.ru>
2019-04-19 11:52:48 +0400

    Adding arch scripts

--- bootstrap_run.sh
+++ bootstrap_run.sh
@@ -1,23 +1,23 @@
-sudo add-apt-repository ppa:openjdk-r/ppa
-sudo apt update
-sudo apt-get install -y -q libavformat-dev libjpeg8 libpng12-0 libtiff5 libjasper1 libtbb2 libavcodec-dev libavfilter-dev libswscale-dev libopenexr-dev 
-sudo apt-get install libopencv-dev ffmpeg 
-sudo apt install git audacity
-sudo apt install openjdk-8-jdk
-sudo apt install openjdk-11-jdk
-chmod 777 8.sh 11.sh
+#sudo add-apt-repository ppa:openjdk-r/ppa
+#sudo apt update
+#sudo apt-get install -y -q libavformat-dev libjpeg8 libpng12-0 libtiff5 libjasper1 libtbb2 libavcodec-dev libavfilter-dev libswscale-dev libopenexr-dev 
+#sudo apt-get install libopencv-dev ffmpeg 
+#sudo apt install git audacity
+#sudo apt install openjdk-8-jdk
+#sudo apt install openjdk-11-jdk
+#chmod 777 8.sh 11.sh
 #wget https://download.java.net/java/GA/jdk11/13/GPL/openjdk-11.0.1_linux-x64_bin.tar.gz
 #tar xf  openjdk-11.0.1_linux-x64_bin.tar.gz
 #cd  jdk-11.0.1/
 #sudo cp -R ./* /usr/lib/jvm/
 #cd ../
-sudo apt install maven
+#sudo apt install maven
 git clone https://github.com/Roland4444/smevmessage
-./8.sh
+~/8.sh
 cd smevmessage
 mvn clean install package
 cd ../
-./11.sh
+~/11.sh
 git clone https://github.com/Roland4444/JAKtor
 cd JAKtor
 mvn clean install package
@@ -34,6 +34,6 @@ cd ../
 git clone https://github.com/Roland4444/gui_ebs_client
 cd gui_ebs_client/
 mvn clean install package
-sudo cp ./So/* /usr/lib
+#sudo cp ./So/* /usr/lib
 ./run.sh
 
